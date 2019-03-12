bac7d730d3862d1874b09317d29219df95887c8e
Kaibito <gpuzzletime@icloud.com>
2019-03-13 00:11:29 +0300

    thesis-1: more tokenizer tests

--- feast-template/test/tokenizer/addons/tags.tests.ts
+++ feast-template/test/tokenizer/addons/tags.tests.ts
@@ -20,6 +20,81 @@ import {
 } from "../../../src/tokenizer/types";
 
 
+describe('tokenizer tags addon: attributes', () => {
+
+	test('no value, no spaces', () => {
+		expect(
+			tokenize('attr/>', {debug: true})
+		).toMatchObject([
+			{
+				type: TOKEN_STRING,
+				start: {index: 0, offset: 1},
+				end: {index: 4, offset: 5},
+				value: 'attr',
+			},
+			{
+				type: TOKEN_FORWARD_SLASH,
+				start: {index: 4, offset: 5},
+				end: {index: 5, offset: 6},
+				value: '/',
+			},
+			{type: TOKEN_TAG_CLOSE}
+		]);
+	});
+
+	test('no value, one space', () => {
+		expect(
+			tokenize('attr />', {debug: true})
+		).toMatchObject([
+			{
+				type: TOKEN_STRING,
+				start: {index: 0, offset: 1},
+				end: {index: 4, offset: 5},
+				value: 'attr',
+			},
+			{
+				type: TOKEN_FORWARD_SLASH,
+				start: {index: 5, offset: 6},
+				end: {index: 6, offset: 7},
+				value: '/',
+			},
+			{type: TOKEN_TAG_CLOSE}
+		]);
+	});
+
+	test('value, no spaces', () => {
+		expect(
+			tokenize('attr=value/>', {debug: true})
+		).toMatchObject([
+			{
+				type: TOKEN_STRING,
+				start: {index: 0, offset: 1},
+				end: {index: 4, offset: 5},
+				value: 'attr',
+			},
+			{
+				type: TOKEN_ASSIGN,
+				start: {index: 4, offset: 5},
+				end: {index: 5, offset: 6},
+				value: '=',
+			},
+			{
+				type: TOKEN_STRING,
+				start: {index: 5, offset: 6},
+				end: {index: 10, offset: 11},
+				value: 'value',
+			},
+			{
+				type: TOKEN_FORWARD_SLASH,
+				start: {index: 10, offset: 11},
+				end: {index: 11, offset: 12},
+				value: '/',
+			},
+			{type: TOKEN_TAG_CLOSE}
+		]);
+	});
+});
+
 describe('tokenizer tags addon: spaces', () => {
 
 	test('no spaces', () => {
@@ -28,58 +103,26 @@ describe('tokenizer tags addon: spaces', () => {
 		).toMatchObject([
 			{
 				type: TOKEN_TAG_OPEN,
-				start: {
-					index: 0,
-					line: 1,
-					offset: 1,
-				},
-				end: {
-					index: 1,
-					line: 1,
-					offset: 2,
-				},
+				start: {index: 0, offset: 1},
+				end: {index: 1, offset: 2},
 				value: '<',
 			},
 			{
 				type: TOKEN_STRING,
-				start: {
-					index: 1,
-					line: 1,
-					offset: 2,
-				},
-				end: {
-					index: 4,
-					line: 1,
-					offset: 5,
-				},
+				start: {index: 1, offset: 2},
+				end: {index: 4, offset: 5},
 				value: 'tag',
 			},
 			{
 				type: TOKEN_FORWARD_SLASH,
-				start: {
-					index: 4,
-					line: 1,
-					offset: 5,
-				},
-				end: {
-					index: 5,
-					line: 1,
-					offset: 6,
-				},
+				start: {index: 4, offset: 5},
+				end: {index: 5, offset: 6},
 				value: '/',
 			},
 			{
 				type: TOKEN_TAG_CLOSE,
-				start: {
-					index: 5,
-					line: 1,
-					offset: 6,
-				},
-				end: {
-					index: 6,
-					line: 1,
-					offset: 7,
-				},
+				start: {index: 5, offset: 6},
+				end: {index: 6, offset: 7},
 				value: '>',
 			},
 		]);
@@ -91,58 +134,26 @@ describe('tokenizer tags addon: spaces', () => {
 		).toMatchObject([
 			{
 				type: TOKEN_TAG_OPEN,
-				start: {
-					index: 1,
-					line: 1,
-					offset: 2,
-				},
-				end: {
-					index: 2,
-					line: 1,
-					offset: 3,
-				},
+				start: {index: 1, offset: 2},
+				end: {index: 2, offset: 3},
 				value: '<',
 			},
 			{
 				type: TOKEN_STRING,
-				start: {
-					index: 3,
-					line: 1,
-					offset: 4,
-				},
-				end: {
-					index: 6,
-					line: 1,
-					offset: 7,
-				},
+				start: {index: 3, offset: 4},
+				end: {index: 6, offset: 7},
 				value: 'tag',
 			},
 			{
 				type: TOKEN_FORWARD_SLASH,
-				start: {
-					index: 7,
-					line: 1,
-					offset: 8,
-				},
-				end: {
-					index: 8,
-					line: 1,
-					offset: 9,
-				},
+				start: {index: 7, offset: 8},
+				end: {index: 8, offset: 9},
 				value: '/',
 			},
 			{
 				type: TOKEN_TAG_CLOSE,
-				start: {
-					index: 9,
-					line: 1,
-					offset: 10,
-				},
-				end: {
-					index: 10,
-					line: 1,
-					offset: 11,
-				},
+				start: {index: 9, offset: 10},
+				end: {index: 10, offset: 11},
 				value: '>',
 			},
 		]);
@@ -154,58 +165,26 @@ describe('tokenizer tags addon: spaces', () => {
 		).toMatchObject([
 			{
 				type: TOKEN_TAG_OPEN,
-				start: {
-					index: 2,
-					line: 1,
-					offset: 3,
-				},
-				end: {
-					index: 3,
-					line: 1,
-					offset: 4,
-				},
+				start: {index: 2, offset: 3},
+				end: {index: 3, offset: 4},
 				value: '<',
 			},
 			{
 				type: TOKEN_STRING,
-				start: {
-					index: 5,
-					line: 1,
-					offset: 6,
-				},
-				end: {
-					index: 8,
-					line: 1,
-					offset: 9,
-				},
+				start: {index: 5, offset: 6},
+				end: {index: 8, offset: 9},
 				value: 'tag',
 			},
 			{
 				type: TOKEN_FORWARD_SLASH,
-				start: {
-					index: 10,
-					line: 1,
-					offset: 11,
-				},
-				end: {
-					index: 11,
-					line: 1,
-					offset: 12,
-				},
+				start: {index: 10, offset: 11},
+				end: {index: 11, offset: 12},
 				value: '/',
 			},
 			{
 				type: TOKEN_TAG_CLOSE,
-				start: {
-					index: 13,
-					line: 1,
-					offset: 14,
-				},
-				end: {
-					index: 14,
-					line: 1,
-					offset: 15,
-				},
+				start: {index: 13, offset: 14},
+				end: {index: 14, offset: 15},
 				value: '>',
 			},
 		]);
